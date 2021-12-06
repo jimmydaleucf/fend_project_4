8d1a327869d5273234aea2763d1591e320373a0f
Jimmy Dale <58115871+jimmydaleucf@users.noreply.github.com>
2021-12-06 15:29:21 -0500

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,17 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+// import './styles/resets.scss'
+// import './styles/base.scss'
+// import './styles/header.scss'
+// import './styles/form.scss'
+// import './styles/footer.scss'
+
+console.log(checkForName);
+console.log(handleSubmit);
+
+
+alert('I EXIST')
+console.log('CHANGE!!');
+
+
---
+++ src/server/index.js
@@ -0,0 +1,27 @@
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+
+const app = express()
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+// var textapi = new aylien({
+//   application_key: process.env.API_KEY,
+// });
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+    // res.sendFile(path.resolve('src/client/views/index.html')) {{removing as we now need to reference the dist folder file}}
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
