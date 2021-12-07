b971e8a1f0e23a19d3a7d8dbbe4dc958b902c07b
Jimmy Dale <jimmy.dale@warnermedia.com>
2021-12-07 10:30:09 -0500

    API call working, response returned

--- src/client/js/analyzeThis.js
+++ src/client/js/analyzeThis.js
@@ -1,25 +1,39 @@
 let baseURl= "https://api.meaningcloud.com/sentiment-2.1?"
 let input = document.getElementById("input");
 
+async function analyzeThis() {
+  const formdata = new FormData();
+  formdata.append("key", "fc01f7cc1b734751ec308977748b84d3");
+  formdata.append(
+    "txt",
+    "This is a test message to see if the boilerplate API call works"
+  );
+  formdata.append("lang", "en"); // 2-letter code, like en es fr ...
 
-const formdata = new FormData();
-formdata.append("key", "fc01f7cc1b734751ec308977748b84d3");
-formdata.append("txt", "This is a test message to see if the boilerplate API call works");
-formdata.append("lang", "en"); // 2-letter code, like en es fr ...
+  const requestOptions = {
+    method: "POST",
+    body: formdata,
+    redirect: "follow",
+  };
 
-const requestOptions = {
-  method: "POST",
-  body: formdata,
-  redirect: "follow",
-};
-
-function analyzeThis(){
-const response = fetch("https://api.meaningcloud.com/sentiment-2.1?key=fc01f7cc1b734751ec308977748b84d3&lang=en&txt=This is a hardcoded message to see if i can get the api to work.")
-  .then(response => ({ 
-    body: response.json()
-  }))
-  .then(({body }) => console.log(body))
-  .catch(error => console.log('error', error));
+  const response = await fetch(
+    "https://api.meaningcloud.com/sentiment-2.1",requestOptions) //?key=fc01f7cc1b734751ec308977748b84d3&lang=en&txt=This is a hardcoded message to see if i can get the api to work.")
+    try {
+        const results = await response.json();
+        console.log(results);
+        return results
+    }  
+    catch(error) {
+        console.log("error", error);
+    }
+  
+//   .then((response) => ({
+//       status: response.status,
+//       body: response.json(),
+//     }))
+//     .then(({ body, status }) => console.log(status, body))
+    
+// }
 }
 
 export {analyzeThis}
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -7,12 +7,12 @@ function handleSubmit(event) {
     let formText = document.getElementById('input').value
     Client.analyzeThis(formText)
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    // console.log("::: Form Submitted :::")
+    // fetch('http://localhost:8080/test')
+    // .then(res => res.json())
+    // .then(function(res) {
+    //     document.getElementById('results').innerHTML = res.message
+    // })
 }
 
 export { handleSubmit }
