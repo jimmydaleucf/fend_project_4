264640eeafc63616b29d033916822d768c1496c3
Jimmy Dale <jimmy.dale@warnermedia.com>
2021-12-08 17:21:36 -0500

    WORKING! Results populated upon submission

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -3,7 +3,10 @@ const updateUI = async (url = "") => {
   try {
     const allData = await response.json();
     console.log(allData);
-    
+    document.getElementById('agree').innerHTML="Agreement: "+ allData.agreement
+    document.getElementById("subj").innerHTML ="Subjectivity: " + allData.subjectivity;
+    document.getElementById("irony").innerHTML ="Irony: " + allData.irony;
+    document.getElementById("confidence").innerHTML ="Confidence: " + allData.confidence;
   } catch (error) {
     console.log("error", error);
   }
