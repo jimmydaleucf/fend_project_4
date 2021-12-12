0776432945bfc01bb9d7b2f06185e10d7cfb0ff7
Jimmy Dale <jimmy.dale@warnermedia.com>
2021-12-12 12:53:25 -0500

    FInally done!

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,6 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const WorkboxPlugin = require("workbox-webpack-plugin")
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -32,7 +31,6 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
-    new WorkboxPlugin.GenerateSW(),
     new CleanWebpackPlugin({
       // Simulate the removal of files
       dry: true,
